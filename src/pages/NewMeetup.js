857a158ae784918023ffb567417f74a669037813
Vanessa <interntemp21@gmail.com>
2021-12-20 17:24:47 +0800

    Create New Meetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,4 +1,11 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>New Meetups Page</div>;
+    return (
+        <section>
+            <h1>New Meetups Page</h1>
+            <NewMeetupForm />
+        </section>
+    );
 }
 export default NewMeetupPage;
