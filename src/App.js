9c25ab4b92900a36ff59bd75663f4da4d71dac28
Vanessa <interntemp21@gmail.com>
2021-12-20 16:34:13 +0800

    layout css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import {Route, Routes} from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from "./components/layout/MainNavigation";
+// import MainNavigation from "./components/layout/MainNavigation";
 import React from "react";
+import Layout from "./components/layout/Layout";
 
 
 
 function App() {
-  return <div>
-      <MainNavigation/>
+  return <Layout>
+      {/*<MainNavigation/>*/}
       <Routes>
           {/*<Route path="/" element={<App/>}/>*/}
           <Route path="/" element={<AllMeetupsPage/>}/>
           <Route path="/new-meetup" element={<NewMeetupPage/>}/>
           <Route path="/fav" element={<FavoritesPage/>}/>
       </Routes>
-  </div>;
+  </Layout>;
 }
 
 export default App;
