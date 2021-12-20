98a153976879d01aa329d6f0e1a135c0c8a8dfa0
Vanessa <interntemp21@gmail.com>
2021-12-20 14:57:19 +0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import {Route, Routes} from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from "./components/layout/MainNavigation";
+import React from "react";
 
 function App() {
-  return <div></div>;
+  return <div>
+      <MainNavigation/>
+      <Routes>
+          {/*<Route path="/" element={<App/>}/>*/}
+          <Route path="/" element={<AllMeetupsPage/>}/>
+          <Route path="/new-meetup" element={<NewMeetupPage/>}/>
+          <Route path="/fav" element={<FavoritesPage/>}/>
+      </Routes>
+  </div>;
 }
 
 export default App;
